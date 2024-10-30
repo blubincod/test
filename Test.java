c4f34aaf963a27fc0a4d6e9d24f5ca1795118fbc
Jina <162255686+zhdiddl@users.noreply.github.com>
2024-10-30 21:25:36 +0900

    Revert "Test5"

--- Test.java
+++ Test.java
@@ -1,7 +1,2 @@
 public class Test {
-    String test = "준엽님이 develop에서 test2 브랜치를 따서 작업하고 PR 생성한 상태 (merge 전)" asdfasdfasdfasdfasfd
-
-    System
-
-    test 5 : (test3 merge 전, test2랑 test4 내용만 포함한 상태)
 }
